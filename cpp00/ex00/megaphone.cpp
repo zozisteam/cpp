7bebc8016afaa0d5f68bd2a052c8c0876f28bda7
Alyazia Almazrouei <alalmazr@lab1r1s12.42abudhabi.ae>
2022-12-02 20:57:13 +0400

    lol

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alalmazr <alalmazr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 19:57:57 by alalmazr          #+#    #+#             */
+/*   Updated: 2022/12/02 20:51:12 by alalmazr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void	megaphone(int size, char **str)
+{
+	for (int i = 1; i < size; i++)
+	{
+		std::string s(str[i]);
+
+		for (int j = 0; j < s.length(); j++)
+			std::cout << (char)toupper(s[j]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "BOOB" << std::endl;
+		return 0;
+	}
+	megaphone(argc, argv);
+	return 0;
+}	
