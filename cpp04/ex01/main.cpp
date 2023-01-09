b6418b4986d2678dade42ddfe37f899a2473c9b5
zozi macbook <y.700@live.com>
2023-01-09 10:12:34 +0400

    zz

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alalmazr <alalmazr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 15:47:43 by alalmazr          #+#    #+#             */
-/*   Updated: 2023/01/05 17:07:49 by alalmazr         ###   ########.fr       */
+/*   Updated: 2023/01/06 16:30:41 by alalmazr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,9 @@ int main() {
 	 for (int i = 0; i < 3; i++) { 
 
 		 delete animals[i]; //deletes the animal object, calls the destructor of the derived class first, then the base class destructor.  
-std::cout << std::endl;
-	 }  
+	std::cout << std::endl << std::endl << std::endl;
+
+
+	}  
 
  }
